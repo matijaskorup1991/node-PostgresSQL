184c4360e44f335d469ec4d5a09bb74f4350952c
matijaskorup1991 <smatija1991@gmail.com>
2021-04-26 15:58:43 +0200

    routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.get("/api/restaurants", (req, res) => {
+  res.send("test");
+});
+app.post("/api/restaurants", (req, res) => {});
+app.get("/api/restaurants/:id", (req, res) => {});
+app.put("/api/restaurants", (req, res) => {});
+app.delete("/api/restaurants", (req, res) => {});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Server listening on port: " + PORT);
