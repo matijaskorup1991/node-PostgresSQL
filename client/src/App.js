16e5639bee96a083d1f261a9ab3a7128fa55cd4f
matijaskorup1991 <smatija1991@gmail.com>
2021-04-27 14:16:39 +0200

    client2

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Switch, Router } from "react-router-dom";
+import Home from "./routes/Home";
+import RestaurantDetailsPage from "./routes/RestaurantDetailsPage";
+import UpdatePage from "./routes/UpdatePage";
 
 function App() {
-  return <div>test</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/restaurants/:id/update' component={UpdatePage} />
+          <Route
+            exact
+            path='/restaurants/:id'
+            component={RestaurantDetailsPage}
+          />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/routes/RestaurantDetailsPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RestaurantDetailsPage = () => {
+  return <div>Restaurant</div>;
+};
+
+export default RestaurantDetailsPage;
